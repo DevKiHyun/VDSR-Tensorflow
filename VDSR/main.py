b8364ff435bf4bc6e707a9357d705dc01b745dca
DevKiHyun <gunatenam@naver.com>
2018-09-27 23:36:54 +0900

    Update main.py
    changed main code

--- VDSR/main.py
+++ VDSR/main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser.add_argument('--learning_rate', type=float, default=0.0001, help='-')
     parser.add_argument('--training_epoch', type=int, default=80, help='-')
     parser.add_argument('--batch_size', type=int, default=64, help='-')
-    parser.add_argument('--n_channel', type=int, default=1, help='-')
+    parser.add_argument('--n_channel', type=int, default=3, help='-')
     parser.add_argument('--grad_clip', type=float, default=1e-1, help='-')
     parser.add_argument('--on_grad_clipping', type=bool, default=False, help='-')
     args, unknown = parser.parse_known_args()
